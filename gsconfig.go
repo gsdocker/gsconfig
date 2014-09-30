72c2c70183aa5195c3bb9638017e8759142d4240
yayanyang <yayanyang@gmail.com>
2014-09-30 14:50:21 +0800

    init

---
+++ gsconfig.go
@@ -0,0 +1,137 @@
+package gsconfig
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/gsdocker/gserrors"
+)
+
+//gsconfig package's public errors
+var (
+	ErrConvert = errors.New("config object convert error")
+)
+
+var config map[string]string
+
+func init() {
+	config = make(map[string]string)
+}
+
+//Save save as global config
+func Save(kvs map[string]string) {
+	config = kvs
+}
+
+//Get .
+func Get(key string) (val string, ok bool) {
+
+	val, ok = config[key]
+
+	return
+}
+
+//String .
+func String(key string, defaultval string) string {
+	val, ok := Get(key)
+
+	if ok {
+
+		return val
+	}
+
+	return defaultval
+}
+
+//Int64 .
+func Int64(key string, defaultval int64) int64 {
+	val, ok := Get(key)
+
+	if ok {
+		i, err := strconv.ParseInt(val, 0, 64)
+
+		if err != nil {
+			gserrors.Panic(ErrConvert)
+		}
+
+		return i
+	}
+
+	return defaultval
+}
+
+//Int32 .
+func Int32(key string, defaultval int32) int32 {
+	return int32(Int64(key, int64(defaultval)))
+}
+
+//Int16 .
+func Int16(key string, defaultval int16) int16 {
+	return int16(Int64(key, int64(defaultval)))
+}
+
+//Uint64 .
+func Uint64(key string, defaultval uint64) uint64 {
+	val, ok := Get(key)
+
+	if ok {
+		i, err := strconv.ParseUint(val, 0, 64)
+
+		if err != nil {
+			gserrors.Panic(ErrConvert)
+		}
+
+		return i
+	}
+
+	return defaultval
+}
+
+//Uint32 .
+func Uint32(key string, defaultval uint32) uint32 {
+	return uint32(Uint64(key, uint64(defaultval)))
+}
+
+//Uint16 .
+func Uint16(key string, defaultval uint16) uint16 {
+	return uint16(Uint64(key, uint64(defaultval)))
+}
+
+//Bool .
+func Bool(key string, defaultval bool) bool {
+	val, ok := Get(key)
+
+	if ok {
+		i, err := strconv.ParseBool(val)
+
+		if err != nil {
+			gserrors.Panic(ErrConvert)
+		}
+
+		return i
+	}
+
+	return defaultval
+}
+
+//Float64 .
+func Float64(key string, defaultval float64) float64 {
+	val, ok := Get(key)
+
+	if ok {
+		i, err := strconv.ParseFloat(val, 64)
+
+		if err != nil {
+			gserrors.Panic(ErrConvert)
+		}
+
+		return i
+	}
+
+	return defaultval
+}
+
+//Float32 .
+func Float32(key string, defaultval float32) float32 {
+	return float32(Float64(key, float64(defaultval)))
+}
